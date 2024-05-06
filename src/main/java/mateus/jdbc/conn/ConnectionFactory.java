21a2c00c386b215a1b3ea0432b324d71a57238e5
Mateus Lopes <mateus_tiwu@hotmail.com>
2024-05-06 11:03:06 -0500

    Connecting MySQL DB

--- src/main/java/mateus/jdbc/conn/ConnectionFactory.java
+++ src/main/java/mateus/jdbc/conn/ConnectionFactory.java
@@ -1,5 +1,20 @@
 package mateus.jdbc.conn;
 
-public class ConnectionFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionFactory {
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3307/anime_store";
+        String username = "root";
+        String password = "password";
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
---
+++ src/main/java/mateus/jdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package mateus.jdbc.test;
+
+import mateus.jdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
